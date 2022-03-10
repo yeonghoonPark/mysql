1ea49f1422b31525e57fb8abe0471944dd028e4a
yeonghoonPark <95198410+yeonghoonPark@users.noreply.github.com>
2022-03-10 18:08:11 +0900

    Add files via upload

---
+++ mysql/연습6 권한.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS study;
+
+CREATE DATABASE study;
+
+/* 유저 생성 후 주석처리 */
+-- CREATE USER study@localhost IDENTIFIED WITH mysql_native_password by 'study';
+
+/* 권한 부여 후 주석처리 */
+-- GRANT ALL PRIVILEGES ON study.* TO study@localhost WITH GRANT OPTION;
+
+
+
+
---
+++ mysql/연습7 권한.sql
@@ -0,0 +1,8 @@
+-- DROP USER study@localhost;
+-- DROP DATABASE study;
+
+/* CREATE USER 아이디@어드레스 IDENTIFIED WITH mysql_native_password BY '패스워드'; */
+
+-- CREATE USER park@localhost IDENTIFIED WITH mysql_native_password BY 'park';
+
+-- GRANT ALL PRIVILEGES ON madang.* TO park@localhost WITH GRANT OPTION;
